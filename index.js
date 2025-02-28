cfbcd152050ff265ae55f60ee0fb3cb874ba3444
soviet <soviet@cas.com.kh>
2025-02-28 17:00:36 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const WebSocket = require("ws");
+const { v4: uuidv4 } = require("uuid");  // Generates unique IDs
+
+const server = new WebSocket.Server({ port: 8080 });
+const clients = {};  // Store Remote ID → WebSocket mapping
+
+server.on("connection", (socket) => {
+    const remoteId = uuidv4().split('-')[0]; // Generate a short unique ID
+    clients[remoteId] = socket;
+    socket.send(JSON.stringify({ remoteId })); // Send Remote ID to client
+
+    socket.on("message", (message) => {
+        const data = JSON.parse(message);
+
+        if (data.to && clients[data.to]) {
+            clients[data.to].send(JSON.stringify(data));
+        }
+    });
+
+    socket.on("close", () => {
+        delete clients[remoteId];
+    });
+});
+
+console.log("WebSocket server running on ws://localhost:8080");
